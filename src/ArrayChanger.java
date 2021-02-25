0e169ed921b307bf5fd851a69bdabf1374e643b8
Souljeee <drobitek76106055@gmail.com>
2021-02-25 10:50:43 +0300

    homework done

---
+++ src/ArrayChanger.java
@@ -0,0 +1,65 @@
+public class ArrayChanger {
+
+    //Метод 1
+
+    public static void arrayChange1(){
+        final int SIZE = 10000000;
+        float[] arr = new float[SIZE];
+
+        for (int i = 0; i < SIZE-1; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < SIZE-1; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    //Метод 2
+
+    public static void arrayChange2(){
+        final int SIZE = 10000000;
+        final int HALF = SIZE / 2;
+        float[] arr = new float[SIZE];
+        float[] a1 = new float[HALF];
+        float[] a2 = new float[HALF];
+
+        for (int i = 0; i < SIZE-1; i++) {
+            arr[i] = 1;
+        }
+
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF-1; i++) {
+                    a1[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF-1; i++) {
+                    a2[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, HALF);
+        System.arraycopy(arr, HALF-1, a2, 0, HALF);
+
+        thread1.start();
+        thread2.start();
+
+        System.arraycopy(a1, 0, arr, 0, HALF);
+        System.arraycopy(a2, 0, arr, HALF, HALF);
+
+        System.out.println(System.currentTimeMillis() - a);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println("------------МЕТОД 1------------");
+        ArrayChanger.arrayChange1();
+        System.out.println("-------------------------------");
+
+        System.out.println("------------МЕТОД 2------------");
+        ArrayChanger.arrayChange2();
+        System.out.println("-------------------------------");
+    }
+
+
+}
+
