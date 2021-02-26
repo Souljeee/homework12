9a0e48efaf1d8cb862309de55cd379ecd7d4cad4
Souljeee <drobitek76106055@gmail.com>
2021-02-26 21:57:55 +0300

    homework done

--- src/ArrayChanger.java
+++ src/ArrayChanger.java
@@ -6,11 +6,11 @@ public class ArrayChanger {
         final int SIZE = 10000000;
         float[] arr = new float[SIZE];
 
-        for (int i = 0; i < SIZE-1; i++) {
+        for (int i = 0; i < SIZE; i++) {
             arr[i] = 1;
         }
         long a = System.currentTimeMillis();
-        for (int i = 0; i < SIZE-1; i++) {
+        for (int i = 0; i < SIZE; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println(System.currentTimeMillis() - a);
